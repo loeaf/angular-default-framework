cdd4e5b7c500df3c33bd3365b33a6e9ea9c44973
loeaf <ckdic8524@naver.com>
2022-02-12 22:36:52 +0900

    구글 아이콘 로고 추가

---
+++ src/app/components/vector/logos/google-icon/google-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-google-icon',
+  templateUrl: './google-icon.component.html',
+  styleUrls: ['./google-icon.component.css']
+})
+export class GoogleIconComponent {
+
+}
---
+++ src/app/components/vector/logos/google-icon/google-icon.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GoogleIconComponent } from './google-icon.component';
+
+
+
+@NgModule({
+  declarations: [
+    GoogleIconComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GoogleIconModule { }
