aaac130f09dbbf33df4ebdf3b19ebd793699ce00
loeaf <ckdic8524@naver.com>
2022-02-13 22:37:12 +0900

    구글 로고 추가

---
+++ src/app/components/vector/logos/google-logo/google-logo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-google-logo',
+  templateUrl: './google-logo.component.html',
+  styleUrls: ['./google-logo.component.css']
+})
+export class GoogleLogoComponent {
+
+}
---
+++ src/app/components/vector/logos/google-logo/google-logo.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GoogleLogoComponent } from './google-logo.component';
+
+
+
+@NgModule({
+  declarations: [
+    GoogleLogoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GoogleLogoModule { }
